98a70aa5089ec474fb7230c679c55f93a82e2a05
rockbarrera <rodrigo.barbernal@gmail.com>
2018-05-28 09:19:50 -0500

    Piso

--- Proyecto Final CGA/src/main.cpp
+++ Proyecto Final CGA/src/main.cpp
@@ -228,10 +228,10 @@ void applicationLoop() {
 
 	glm::vec3 cubePositions[] =
 	{ glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f, 0.0f, 1.0f),
-		glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(-1.0f, 0.0f, -1.0f),
-		glm::vec3(4.4f, -0.4f, -3.5f), glm::vec3(-6.7f, 3.0f, -7.5f),
-		glm::vec3(-4.3f, -3.0f, -2.5f), glm::vec3(3.5f, 8.0f, -2.5f),
-		glm::vec3(-1.7f, -0.7f, -1.5f), glm::vec3(3.3f, 3.0f, -1.5f) };
+		glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(1.0f, 0.0f, 1.0f),
+		glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f),
+		glm::vec3(-1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 0.0f, -1.0f),
+		glm::vec3(-1.0f, 0.0f, 1.0f), glm::vec3(1.0f, 0.0f, -1.0f) };
 
 	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 	double lastTime = TimeManager::Instance().GetTime();
@@ -395,10 +395,10 @@ void applicationLoop() {
 		//glm::vec3(1.0f, 0.0f, 0.0f));
 		glBindVertexArray(VAO);
 
-		for (GLint i = 0; i < 4; i++) {
+		for (GLint i = 0; i < 9; i++) {
 			glm::mat4 model;
 			model = glm::translate(model, cubePositions[i]);
-			model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			//model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
